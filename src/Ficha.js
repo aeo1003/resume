90faf560e9b7ecc12ad746b6ded185c46b6eb618
aeo1003 <aeo1003@gmail.com>
2022-11-07 16:54:45 +0100

    csss espacio entre trabajos

--- src/Ficha.js
+++ src/Ficha.js
@@ -3,11 +3,10 @@ import Calendar from './img/calendar-svgrepo-com.svg'
 
 function Ficha(props) {
   return (
-    <div>
-        <p></p>
+    <div style={{marginTop:'30px'}}>
         <div className='puesto'>{props.puesto}</div>
         <div className='empresa'>{props.empresa}</div>
-        <div className='fecha'><img alt="" src={Calendar} width='20px' />{props.fechas}</div>
+        <div className='fecha'><img alt="" src={Calendar} width='20px' style={{marginBottom: '10px'}} />{props.fechas}</div>
         <ul>{props.datos.map((d,index)=> (<li  className='trabajo' key={index}> {d} </li>))}</ul>
     </div>
   )
